092d7db7460c43d153851f22e496fd0a7a8cb84e
zhidong <517341003@qq.com>
2018-12-31 23:52:54 +0800

    zstore-oder

---
+++ zstore-oder/src/main/java/per/zd/zstore/util/DiscoveryUtil.java
@@ -0,0 +1,28 @@
+package per.zd.zstore.util;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DiscoveryUtil {
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${server.port}")
+    private String ip;
+
+    @GetMapping("/oder")
+    public String client()
+    {
+        String services = "appName : "+appName+" ; Services: " + eurekaClient.getApplication("zstore-user").getInstances().iterator()+" ip :"+ip;
+        System.out.println(services);
+        return services;
+    }
+}
